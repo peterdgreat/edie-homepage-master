d6cda7ce64cc2d2a73129f01bb01d7d102e47f6c
Ajayi Peter <peterdgreat2016@gmail.com>
2020-07-25 15:12:21 +0100

    building JS environment

---
+++ server/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const path = require("path");
+const open = require("open");
+
+import webpack from "webpack";
+import config from "../webpack.config.dev.js";
+
+const port = 8001;
+const app = express();
+const compiler = webpack(config);
+
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+  })
+);
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/src/index.html"));
+});
+
+app.get("/users", function (req, res) {
+  // Mocking an API call
+  res.json([
+    {
+      id: 1,
+      firstName: "John",
+      lastName: "Smith",
+      email: "john.smith@gmail.com",
+    },
+    {
+      id: 2,
+      firstName: "Tina",
+      lastName: "Lee",
+      email: "tina.lee@gmail.com",
+    },
+    {
+      id: 3,
+      firstName: "Jane",
+      lastName: "Doe",
+      email: "jane.doe@gmail.com",
+    },
+  ]);
+});
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open("http://localhost:" + port);
+  }
+});
